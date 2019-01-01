4801f1002efc9d9ccc4757c94d045fcac257282d
Adrian Lopez <adrian@imagendigital.com>
2019-01-01 14:46:17 -0600

    save

--- public/js/practicas/render.js
+++ public/js/practicas/render.js
@@ -1,16 +1,18 @@
- const TemplateExample = ( { title , summary , images, url } )=>{
+ const TemplateExample = ( { title , summary , images, url , num , id  } )=>{
  	let realSummary = summary.substring(0,50);
  	return `
  	<div class="card">
  	<div class="card-image">
- 	<img src="${images.principal[0].url}" alt>          
+ 	<img  class='shake' src="${images.principal[0].url}" alt="${title}">          
  	</div>
  	<div class="card-content">
  	<p class='truncate'>${title}</p>
+ 	 	<div class="card-action">
+ 	<a href="${url}" alt='${title }'> Ver <span class="new badge" data-badge-caption=" ">${num}</span>
+  </a>
  	</div>
- 	<div class="card-action">
- 	<a href="${url}"> Ver la nota </a>
  	</div>
+
  	</div>`;
  };
  let settings ={
@@ -25,19 +27,69 @@
  }
  const JsonpApi = new ApiImagen( settings );
  let settingsUrl ={
- 	fields : ['title', 'summary', 'images', 'url' ],
+ 	fields : ['title', 'summary', 'images', 'url', 'id' ],
  	type : ['article','gallerie','gallery','video','videos']
  }
 
 
- JsonpApi.limit = 15;
+ JsonpApi.limit = 5;
+
+
+ let MyNotes = {};
+ MyNotes.Notes = {};
+ MyNotes.Getdata = function( data, type ){
+ 	let MyNote = this.Notes[data.id];
+ 	let All = data.parentElement.querySelectorAll('.demoClass');
+ 	this.Actions(type , All );	
+ 	data.classList.remove('opacitycero');
+ 	
+ }
+ MyNotes.Each = function(dom , callback  ){
+   dom.forEach( function(elem ){
+   	  if(typeof callback === 'function'){
+   	  	callback( elem);
+   	  }
+   })
+ }
+ MyNotes.Actions = function(type, doms){
+ 	switch (type) {
+ 		case 'mouseover':
+ 			this.Each( doms,function( dom ){
+ 				dom.classList.add('opacitycero');
+ 			});
+ 			break;
+
+ 		case 'mouseout':
+ 			this.Each( doms, function(dom ){
+ 				dom.classList.remove('opacitycero');
+ 				
+ 			});
+ 			break;
+ 		default:
+ 			// statements_def
+ 			break;
+ 	}
+ }
+
 
 
 
  let InjectNotes = function( note  ){
+ 	note.num = this.childElementCount + 1;
  	let node = document.createElement( 'div');
- 	node.classList.add('col', 's4');
+ 	node.classList.add('col', 's4', 'demoClass');
+ 	node.id = note.id;
  	node.innerHTML = TemplateExample(note);
+ 	node.addEventListener( 'mouseover',function(event){
+ 	 	console.log( event.type)
+ 	 	MyNotes.Getdata(this, event.type )
+ 	})
+
+ 	node.addEventListener( 'mouseout',function(event){
+ 	 	console.log( event.type)
+ 	 	MyNotes.Getdata(this, event.type )
+ 	})
+ 	MyNotes.Notes[note.id] = Object.assign( {dom :  node }, note);	
  	this.appendChild( node );
  }
 
@@ -53,9 +105,11 @@
  }
 
  document.getElementById('searchMoreNotes')
- .addEventListener( 'click',function(){
+ .addEventListener( 'click',function(){ 	
  	JsonpApi.more(settingsUrl).then(EachNote);
- })
+ });
+
+
 
 
 
